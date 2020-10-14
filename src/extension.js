698142d8361334bf961c5dfb5392760c0ab13007
bxvd <brodie@daff.email>
2020-10-14 22:40:04 +1100

    Convert naming

--- src/extension.js
+++ src/extension.js
@@ -7,32 +7,20 @@ const diff = require('semver/functions/diff');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	this.extensionName = 'RobbOwen.synthwave-vscode';
+
+	this.extensionName = 'bxvd.vt320-vscode';
 	this.cntx = context;
 	this.extension = vscode.extensions.getExtension(this.extensionName);
+
 	if (this.extension) {
+
 		// grab current version number
 		this.version = this.extension.packageJSON.version;
 
-		// grab last recorded version
-		const prevVersion = context.globalState.get(`${this.extensionName}.version`);
-
-		if (prevVersion) {
-			// check it has changed.
-			const d = diff(this.version, prevVersion);
-			// show again on major or minor updates
-			if (d == 'major' || d == 'minor') {
-				showUpdatePage();
-				context.globalState.update(`${this.extensionName}.version`, this.version);
-			}
-		} else {
-			showUpdatePage();
-			context.globalState.update(`${this.extensionName}.version`, this.version);
-		}
-
+		context.globalState.update(`${this.extensionName}.version`, this.version);
 	}
 	
-	const config = vscode.workspace.getConfiguration("synthwave84");
+	const config = vscode.workspace.getConfiguration("vt320");
 
 	let disableGlow = config && config.disableGlow ? !!config.disableGlow : false;
 	
@@ -41,9 +29,9 @@ function activate(context) {
 	brightness = isNaN(brightness) ? 0.45 : brightness;
 
 	const parsedBrightness = Math.floor(brightness * 255).toString(16).toUpperCase();
-	let neonBrightness = parsedBrightness;
+	let glowBrightness = parsedBrightness;
 
-	let disposable = vscode.commands.registerCommand('synthwave84.enableNeon', function () {
+	let disposable = vscode.commands.registerCommand('vt320.enableGlow', function () {
 
 		const isWin = /^win/.test(process.platform);
 		const appDir = path.dirname(require.main.filename);
@@ -58,83 +46,66 @@ function activate(context) {
 		const templateFile =
 				base +
 				(isWin
-					? "\\electron-browser\\workbench\\neondreams.js"
-					: "/electron-browser/workbench/neondreams.js");
+					? "\\electron-browser\\workbench\\phosphorglow.js"
+					: "/electron-browser/workbench/phosphorglow.js");
 
 		try {
-
-			// const version = context.globalState.get(`${context.extensionName}.version`);
 
 			// generate production theme JS
 			const chromeStyles = fs.readFileSync(__dirname +'/css/editor_chrome.css', 'utf-8');
 			const jsTemplate = fs.readFileSync(__dirname +'/js/theme_template.js', 'utf-8');
 			const themeWithGlow = jsTemplate.replace(/\[DISABLE_GLOW\]/g, disableGlow);
 			const themeWithChrome = themeWithGlow.replace(/\[CHROME_STYLES\]/g, chromeStyles);
-			const finalTheme = themeWithChrome.replace(/\[NEON_BRIGHTNESS\]/g, neonBrightness);
+			const finalTheme = themeWithChrome.replace(/\[GLOW_BRIGHTNESS\]/g, glowBrightness);
 			fs.writeFileSync(templateFile, finalTheme, "utf-8");
 			
 			// modify workbench html
 			const html = fs.readFileSync(htmlFile, "utf-8");
 
 			// check if the tag is already there
-			const isEnabled = html.includes("neondreams.js");
+			const isEnabled = html.includes("phosphorglow.js");
 
 			if (!isEnabled) {
-				// delete synthwave script tag if there
-				let output = html.replace(/^.*(<!-- SYNTHWAVE 84 --><script src="neondreams.js"><\/script><!-- NEON DREAMS -->).*\n?/mg, '');
+
+				// delete script tag if there
+				let output = html.replace(/^.*(<!-- DT320 --><script src="phosphorglow.js"><\/script><!-- PHOSPHOR GLOW -->).*\n?/mg, '');
+
 				// add script tag
-				output = html.replace(/\<\/html\>/g, `	<!-- SYNTHWAVE 84 --><script src="neondreams.js"></script><!-- NEON DREAMS -->\n`);
+				output = html.replace(/\<\/html\>/g, `	<!-- DT320 --><script src="phosphorglow.js"></script><!-- PHOSPHOR GLOW -->\n`);
 				output += '</html>';
 	
 				fs.writeFileSync(htmlFile, output, "utf-8");
 				
 				vscode.window
-					.showInformationMessage("Neon Dreams enabled. VS code must reload for this change to take effect. Code may display a warning that it is corrupted, this is normal. You can dismiss this message by choosing 'Don't show this again' on the notification.", { title: "Restart editor to complete" })
+					.showInformationMessage("Glow enabled. VS code must reload for this change to take effect. Code may display a warning that it is corrupted, this is normal. You can dismiss this message by choosing 'Don't show this again' on the notification.", { title: "Restart editor to complete" })
 					.then(function(msg) {
 						vscode.commands.executeCommand("workbench.action.reloadWindow");
 					});
 
 			} else {
 				vscode.window
-					.showInformationMessage('Neon dreams is already enabled. Reload to refresh JS settings.', { title: "Restart editor to refresh settings" })
+					.showInformationMessage('Glow is already enabled. Reload to refresh JS settings.', { title: "Restart editor to refresh settings" })
 					.then(function(msg) {
 						vscode.commands.executeCommand("workbench.action.reloadWindow");
 					});
 			}
 		} catch (e) {
 			if (/ENOENT|EACCES|EPERM/.test(e.code)) {
-				vscode.window.showInformationMessage("You must run VS code with admin priviliges in order to enable Neon Dreams.");
+				vscode.window.showInformationMessage("You must run VS code with admin priviliges in order to enable glow.");
 				return;
 			} else {
-				vscode.window.showErrorMessage('Something went wrong when starting neon dreams');
+				vscode.window.showErrorMessage('Something went wrong when starting glow.');
 				return;
 			}
 		}
 	});
 
-	let disable = vscode.commands.registerCommand('synthwave84.disableNeon', uninstall);
-	let whatsNew = vscode.commands.registerCommand('synthwave84.whatsNew', showUpdatePage);
+	let disable = vscode.commands.registerCommand('vt320.disableGlow', uninstall);
 	
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disable);
-	context.subscriptions.push(whatsNew);
 }
 exports.activate = activate;
-
-
-function showUpdatePage() {
-		const panel = vscode.window.createWebviewPanel(
-			`synthwave.whatsNew`, // Identifies the type of the webview. Used internally
-			'What\'s new for Synthwave \'84', // Title of the panel displayed to the user
-			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-			{ enableScripts: !0 } // Webview options. More on these later.
-		);
-
-		const viewPath = path.join(this.cntx.extensionPath, "whats-new", "view.html");
-		const viewResourcePath = panel.webview.asWebviewUri(viewPath);
-		const htmlContent = fs.readFileSync(viewPath, "utf-8");
-		panel.webview.html = htmlContent;
-}
 
 // this method is called when your extension is deactivated
 function deactivate() {
@@ -155,20 +126,20 @@ function uninstall() {
 	const html = fs.readFileSync(htmlFile, "utf-8");
 
 	// check if the tag is already there
-	const isEnabled = html.includes("neondreams.js");
+	const isEnabled = html.includes("phosphorglow.js");
 
 	if (isEnabled) {
 		// delete synthwave script tag if there
-		let output = html.replace(/^.*(<!-- SYNTHWAVE 84 --><script src="neondreams.js"><\/script><!-- NEON DREAMS -->).*\n?/mg, '');
+		let output = html.replace(/^.*(<!-- DT320 --><script src="phosphorglow.js"><\/script><!-- DT320 -->).*\n?/mg, '');
 		fs.writeFileSync(htmlFile, output, "utf-8");
 
 		vscode.window
-			.showInformationMessage("Neon Dreams disabled. VS code must reload for this change to take effect", { title: "Restart editor to complete" })
+			.showInformationMessage("Glow disabled. VS code must reload for this change to take effect", { title: "Restart editor to complete" })
 			.then(function(msg) {
 				vscode.commands.executeCommand("workbench.action.reloadWindow");
 			});
 	} else {
-		vscode.window.showInformationMessage('Neon dreams isn\'t running.');
+		vscode.window.showInformationMessage('Glow isn\'t running.');
 	}
 }
 
--- src/js/theme_template.js
+++ src/js/theme_template.js
@@ -4,7 +4,7 @@
   const bodyNode = document.querySelector('body');
 
   // Replace the styles with the glow theme
-  const initNeonDreams = (disableGlow, obs) => {
+  const initGlow = (disableGlow, obs) => {
     var themeStyleTag = document.querySelector('.vscode-tokens-styles');
 
     if (!themeStyleTag) {
@@ -17,30 +17,30 @@
     
     if (!disableGlow) {
       /* replace neon red */
-      updatedThemeStyles = updatedThemeStyles.replace(/color: #fe4450;/g, "color: #fff5f6; text-shadow: 0 0 2px #000, 0 0 10px #fc1f2c[NEON_BRIGHTNESS], 0 0 5px #fc1f2c[NEON_BRIGHTNESS], 0 0 25px #fc1f2c[NEON_BRIGHTNESS];");
+      updatedThemeStyles = updatedThemeStyles.replace(/color: #fe4450;/g, "color: #fff5f6; text-shadow: 0 0 2px #000, 0 0 10px #fc1f2c[GLOW_BRIGHTNESS], 0 0 5px #fc1f2c[GLOW_BRIGHTNESS], 0 0 25px #fc1f2c[GLOW_BRIGHTNESS];");
       
       /* replace neon pink */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #ff7edb;/g, "color: #f92aad; text-shadow: 0 0 2px #100c0f, 0 0 5px #dc078e33, 0 0 10px #fff3;");
       
       /* replace yellow */
-      updatedThemeStyles = updatedThemeStyles.replace(/color: #fede5d;/g, "color: #f4eee4; text-shadow: 0 0 2px #393a33, 0 0 8px #f39f05[NEON_BRIGHTNESS], 0 0 2px #f39f05[NEON_BRIGHTNESS];");
+      updatedThemeStyles = updatedThemeStyles.replace(/color: #fede5d;/g, "color: #f4eee4; text-shadow: 0 0 2px #393a33, 0 0 8px #f39f05[GLOW_BRIGHTNESS], 0 0 2px #f39f05[GLOW_BRIGHTNESS];");
       
       /* replace green */
-      updatedThemeStyles = updatedThemeStyles.replace(/color: #72f1b8;/g, "color: #72f1b8; text-shadow: 0 0 2px #100c0f, 0 0 10px #257c55[NEON_BRIGHTNESS], 0 0 35px #212724[NEON_BRIGHTNESS];");
+      updatedThemeStyles = updatedThemeStyles.replace(/color: #72f1b8;/g, "color: #72f1b8; text-shadow: 0 0 2px #100c0f, 0 0 10px #257c55[GLOW_BRIGHTNESS], 0 0 35px #212724[GLOW_BRIGHTNESS];");
       
       /* replace blue */
-      updatedThemeStyles = updatedThemeStyles.replace(/color: #36f9f6;/g, "color: #fdfdfd; text-shadow: 0 0 2px #001716, 0 0 3px #03edf9[NEON_BRIGHTNESS], 0 0 5px #03edf9[NEON_BRIGHTNESS], 0 0 8px #03edf9[NEON_BRIGHTNESS];");
+      updatedThemeStyles = updatedThemeStyles.replace(/color: #36f9f6;/g, "color: #fdfdfd; text-shadow: 0 0 2px #001716, 0 0 3px #03edf9[GLOW_BRIGHTNESS], 0 0 5px #03edf9[GLOW_BRIGHTNESS], 0 0 8px #03edf9[GLOW_BRIGHTNESS];");
     }
 
     /* append the remaining styles */
     updatedThemeStyles = `${updatedThemeStyles}[CHROME_STYLES]`;
 
     const newStyleTag = document.createElement('style');
-    newStyleTag.setAttribute("id", "synthwave-84-theme-styles");
+    newStyleTag.setAttribute("id", "vt320-theme-styles");
     newStyleTag.innerText = updatedThemeStyles.replace(/(\r\n|\n|\r)/gm, '');
     document.body.appendChild(newStyleTag);
     
-    console.log('Synthwave \'84: NEON DREAMS initialised!');
+    console.log('VT320: Phosphor Glow initialised!');
     
     // disconnect the observer because we don't need it anymore
     if (obs) {
@@ -53,33 +53,33 @@
       for(let mutation of mutationsList) {
           if (mutation.type === 'attributes') {
             // only init if we're using a Synthwave 84 subtheme
-            const isUsingSynthwave = document.querySelector('[class*="RobbOwen-synthwave-vscode-themes"]');
+            const isUsingVT320 = document.querySelector('[class*="bxvd-vt320-vscode-themes"]');
             // does the style div exist yet?
             const tokensLoaded = document.querySelector('.vscode-tokens-styles');
             // does it have content ?
             const tokenStyles = document.querySelector('.vscode-tokens-styles').innerText;
 
             // sometimes VS code takes a while to init the styles content, so stop this observer and add an observer for that
-            if (isUsingSynthwave && tokensLoaded) {
+            if (isUsingVT320 && tokensLoaded) {
               observer.disconnect();
               observer.observe(tokensLoaded, { childList: true });
             }
           }
           if (mutation.type === 'childList') {
-            const isUsingSynthwave = document.querySelector('[class*="RobbOwen-synthwave-vscode-themes"]');
+            const isUsingVT320= document.querySelector('[class*="bxvd-vt320-vscode-themes"]');
             const tokensLoaded = document.querySelector('.vscode-tokens-styles');
             const tokenStyles = document.querySelector('.vscode-tokens-styles').innerText;
 
             // Everything we need is ready, so initialise
-            if (isUsingSynthwave && tokensLoaded && tokenStyles) {
-                initNeonDreams([DISABLE_GLOW], observer);
+            if (isUsingVT320 && tokensLoaded && tokenStyles) {
+              initGlow([DISABLE_GLOW], observer);
             }
           }
       }
   };
 
   // try to initialise the theme
-  initNeonDreams([DISABLE_GLOW]);
+  initGlow([DISABLE_GLOW]);
 
   // Use a mutation observer to check when we can bootstrap the theme
   const observer = new MutationObserver(watchForBootstrap);
